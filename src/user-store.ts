224ba32e44e69f86b6c080547d9bd2a25662ab93
Grzegorz Łubian <46636403+glubian@users.noreply.github.com>
2023-03-08 16:53:52 +0100

    Document user store

--- src/user-store.ts
+++ src/user-store.ts
@@ -1,17 +1,23 @@
 import { computed, reactive, watch } from "vue";
 import { i18n } from "./i18n";
 
+/** Language setting. */
 export enum Lang {
   English = "en",
   Polish = "pl",
 }
 
+/** Theme setting. */
 export enum Theme {
   Light = "light",
   Dark = "dark",
   TrueBlack = "",
 }
 
+/**
+ * Preferences stored in `document.cookie`.
+ * `null` and `undefined` values mean lack of preference.
+ */
 interface UserPreferences {
   lang?: Lang | null;
   theme?: Theme | null;
@@ -19,35 +25,42 @@ interface UserPreferences {
 
 const DEFAULT_THEME = defaultTheme();
 
+/** Determines the default theme. */
 function defaultTheme(): Theme {
   const pref = matchMedia("(prefers-color-scheme: dark)").matches;
   return pref ? Theme.Dark : Theme.Light;
 }
 
+/** Current language setting. */
 const currentLang = computed(
   (): Lang => userStore.preferred.lang ?? Lang.English
 );
 
+/** Current theme setting. */
 const currentTheme = computed(
   (): Theme => userStore.preferred.theme ?? DEFAULT_THEME
 );
 
+/** Updates the preferred language and saves changes. */
 function setPreferredLang(lang: Lang | null) {
   userStore.preferred.lang = lang;
   savePreferences();
 }
 
+/** Updates the preferred theme and saves changes. */
 function setPreferredTheme(theme: Theme | null) {
   userStore.preferred.theme = theme;
   savePreferences();
 }
 
+/** Updates CSS theme classes. */
 function applyTheme() {
   const { classList } = document.body;
   classList.toggle(Theme.Dark, userStore.current.theme === Theme.Dark);
   classList.toggle(Theme.Light, userStore.current.theme === Theme.Light);
 }
 
+/** Loads preferences from the cookie. */
 function loadPreferences() {
   if (!document.cookie) {
     return;
@@ -58,6 +71,7 @@ function loadPreferences() {
   userStore.preferred.theme ??= null;
 }
 
+/** Saves preferences to the cookie. */
 function savePreferences() {
   document.cookie = JSON.stringify(userStore.preferred, (_, v) => v ?? void 0);
 }
