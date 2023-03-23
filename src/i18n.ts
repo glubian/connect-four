cee00b9537db0ebc33d8ff5d523cd49c82f3348a
Grzegorz Łubian <46636403+glubian@users.noreply.github.com>
2023-03-24 00:02:04 +0100

    Add translations of seconds

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,9 +1,28 @@
 import { createI18n } from "vue-i18n";
 import messages from "./messages.json";
 
+function polishPluralRules(num: number): number {
+  const SEKUNDA = 0;
+  const SEKUNDY = 1;
+  const SEKUND = 2;
+
+  if (num === 1) {
+    return SEKUNDA;
+  }
+
+  const teen = num > 10 && num < 20;
+  const lastDigit = num % 10;
+  if (!teen && lastDigit > 1 && lastDigit < 5) {
+    return SEKUNDY;
+  }
+
+  return SEKUND;
+}
+
 export const i18n = createI18n({
   legacy: false,
   availableLocales: ["en", "pl"],
+  pluralRules: { pl: polishPluralRules },
   fallbackLocale: "en",
   messages,
   missingWarn: false,
