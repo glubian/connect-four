66e1114eb94d120ceabe4b7e05b3c20a0a2df7f6
Grzegorz Łubian <46636403+glubian@users.noreply.github.com>
2023-04-09 16:51:03 +0200

    Refactor + unwatch after unmounted

--- src/composables/intersection-observer.ts
+++ src/composables/intersection-observer.ts
@@ -1,74 +1,70 @@
-import { shallowRef, triggerRef, watch, type Ref, type ShallowRef } from "vue";
+import {
+  onMounted,
+  onUnmounted,
+  shallowRef,
+  type Ref,
+  type WatchStopHandle,
+  type ShallowRef,
+  triggerRef,
+  watch,
+} from "vue";
 
-type ElementMap = { [key: string]: Ref<HTMLElement | null> };
-type ShallowRefMap<T extends {}, U> = { [key in keyof T]: ShallowRef<U> };
+type ObjectKey = number | string | symbol;
+type Refs = { [key: ObjectKey]: Ref<HTMLElement | null> };
 
-function shallowRefMap<T extends {}, U>(
-  obj: T,
-  value: U
-): Readonly<ShallowRefMap<T, U>> {
-  const res: Partial<ShallowRefMap<T, U>> = {};
-  for (const key of Object.keys(obj) as (keyof T)[]) {
-    res[key] = shallowRef(value);
+interface IntersectionObserverConfig<T extends Refs> {
+  /** Reference to the root element. */
+  rootRef: Ref<HTMLElement | null>;
+  /** References of elements to track. */
+  refs: T;
+  /** Offsets added to each side of the root element's bounding box. */
+  rootMargin?: string;
+  /** Intersection thresholds. */
+  thresholds?: number | number[];
+}
+
+/**
+ * A map with the same set of keys as another object, but values of
+ * a different type.
+ */
+type DerivedMap<T extends {}, U> = { [key in keyof T]: U };
+
+/**
+ * Creates a object, with the same set of keys as `map` and values set using
+ * `initFn` function.
+ */
+function deriveMapFn<T extends {}, U>(
+  map: T,
+  initFn: (key: keyof T) => U
+): DerivedMap<T, U> {
+  const dMap: Partial<DerivedMap<T, U>> = {};
+  for (const key of Object.keys(map) as (keyof T)[]) {
+    dMap[key] = initFn(key);
   }
 
-  return res as ShallowRefMap<T, U>;
+  return dMap as DerivedMap<T, U>;
 }
 
-interface IntersectionObserverConfig<T extends ElementMap> {
-  rootRef: Ref<HTMLElement | null>;
-  refs: T;
-  rootMargin?: string;
-  threshold?: number;
-}
-
-/** Provides a component-friendly interface for `IntersectionObserver`. */
-export function useIntersectionObserver<T extends ElementMap>({
+/** A component-friendly interface for `IntersectionObserver`. */
+export function useIntersectionObserver<T extends Refs>({
   rootRef,
   refs,
   rootMargin,
-  threshold,
+  thresholds,
 }: IntersectionObserverConfig<T>) {
-  const entries: ShallowRefMap<T, IntersectionObserverEntry | null> =
-    shallowRefMap(refs, null);
-  const targetMap = new Map<HTMLElement, (keyof T)[]>();
+  type EntriesMap = DerivedMap<T, ShallowRef<IntersectionObserverEntry | null>>;
+  /** A map of element refs and their current intersection observer entries. */
+  const entries: EntriesMap = deriveMapFn(refs, () => shallowRef(null));
+  /**
+   * Indicates which entries should be updated when an update occurs for a
+   * given element.
+   */
+  const targetMap: Map<HTMLElement, (keyof T)[]> = new Map();
+
+  const handles: WatchStopHandle[] = [];
   let observer: IntersectionObserver | null = null;
 
-  for (const [key, { value }] of Object.entries(refs)) {
-    if (!value) {
-      continue;
-    }
-
-    const keys = targetMap.get(value) ?? [];
-    keys.push(key);
-    targetMap.set(value, keys);
-  }
-
-  for (const [key, value] of Object.entries(refs)) {
-    watch(value, (curr, prev) => {
-      if (prev) {
-        if (observer) {
-          observer.unobserve(prev);
-        }
-
-        targetMap.delete(prev);
-        entries[key].value = null;
-        triggerRef(entries[key]);
-      }
-
-      if (curr) {
-        const keys = targetMap.get(curr) ?? [];
-        keys.push(key);
-        targetMap.set(curr, keys);
-
-        if (observer) {
-          observer.observe(curr);
-        }
-      }
-    });
-  }
-
-  function update(updated: IntersectionObserverEntry[]) {
+  function intersectionObserverCb(updated: IntersectionObserverEntry[]) {
     for (const entry of updated) {
       const keys = targetMap.get(entry.target as HTMLElement);
       if (!keys) {
@@ -82,34 +78,99 @@ export function useIntersectionObserver<T extends ElementMap>({
     }
   }
 
-  watch(
-    rootRef,
-    (curr) => {
+  /** Updates element associated with a reference. */
+  function updateElement(
+    key: keyof T,
+    curr?: HTMLElement | null,
+    prev?: HTMLElement | null
+  ) {
+    if (prev) {
+      unobserveElement(key, prev);
+    }
+
+    if (curr) {
+      const keys = targetMap.get(curr) ?? [];
+      keys.push(key);
+      targetMap.set(curr, keys);
+
       if (observer) {
-        observer.disconnect();
-        observer = null;
-        for (const entry of Object.values(entries)) {
-          entry.value = null;
-          triggerRef(entry);
+        observer.observe(curr);
+      }
+    }
+  }
+
+  /**
+   * Unobserves the element and updates target map and associated entry.
+   * @param key - key of reference which has changed its value
+   * @param el - previous element value
+   */
+  function unobserveElement(key: keyof T, el: HTMLElement) {
+    if (observer) {
+      observer.unobserve(el);
+    }
+
+    const keys = targetMap.get(el);
+    if (keys) {
+      const index = keys.indexOf(key);
+      if (index !== -1) {
+        keys.splice(index, 1);
+      }
+      if (!keys.length) {
+        targetMap.delete(el);
+      }
+    }
+
+    entries[key].value = null;
+  }
+
+  /** Attaches a new observer to a given element (and destroys the old one). */
+  function attachObserver(rootEl: HTMLElement | null) {
+    removeObserver();
+    if (rootEl) {
+      observer = new IntersectionObserver(intersectionObserverCb, {
+        root: rootEl,
+        rootMargin,
+        threshold: thresholds,
+      });
+      for (const { value } of Object.values(refs)) {
+        if (value) {
+          observer.observe(value);
         }
       }
+    }
+  }
 
-      if (curr) {
-        observer = new IntersectionObserver(update, {
-          root: curr,
-          rootMargin,
-          threshold,
-        });
+  /** Disposes of the intersection observer and resets the state. */
+  function removeObserver() {
+    if (observer) {
+      observer.disconnect();
+      observer = null;
+      Object.values(entries).forEach((e) => (e.value = null));
+      targetMap.clear();
+    }
+  }
 
-        for (const { value } of Object.values(refs)) {
-          if (value) {
-            observer.observe(value);
-          }
+  onMounted(() => {
+    for (const [key, value] of Object.entries(refs)) {
+      const handle = watch(
+        value,
+        (curr, prev) => updateElement(key, curr, prev),
+        {
+          immediate: true,
         }
-      }
-    },
-    { immediate: true }
-  );
+      );
+      handles.push(handle);
+    }
+
+    const rootRefHandle = watch(rootRef, attachObserver, { immediate: true });
+    handles.push(rootRefHandle);
+  });
+
+  onUnmounted(() => {
+    removeObserver();
+    handles.forEach((stop) => stop());
+    handles.length = 0;
+  });
 
   return { entries };
 }
--- src/composables/prevent-context-menu.ts
+++ src/composables/prevent-context-menu.ts
@@ -1,26 +1,47 @@
-import { watch, type Ref } from "vue";
+import {
+  watch,
+  type Ref,
+  type WatchStopHandle,
+  onMounted,
+  onUnmounted,
+} from "vue";
 
 /** Prevents context menu from appearing on the referenced element. */
 export function usePreventContextMenu(r: Ref<HTMLElement | null>) {
+  let isEnabled = false;
+  let unwatch: WatchStopHandle | null = null;
+
   function onContextMenu(ev: MouseEvent) {
     if (r.value && r.value === ev.target) {
       ev.preventDefault();
     }
   }
 
-  watch(
-    r,
-    (curr, prev) => {
-      if (curr && prev) {
-        return;
-      }
+  function toggleEventListener(enabled: boolean) {
+    if (enabled === isEnabled) {
+      return;
+    }
 
-      if (!curr) {
-        document.removeEventListener("contextmenu", onContextMenu);
-      } else {
-        document.addEventListener("contextmenu", onContextMenu);
-      }
-    },
-    { immediate: true }
-  );
+    if (enabled) {
+      document.addEventListener("contextmenu", onContextMenu);
+    } else {
+      document.removeEventListener("contextmenu", onContextMenu);
+    }
+
+    isEnabled = enabled;
+  }
+
+  onMounted(() => {
+    unwatch = watch(r, (curr) => toggleEventListener(!!curr), {
+      immediate: true,
+    });
+  });
+
+  onUnmounted(() => {
+    if (unwatch) {
+      unwatch();
+    }
+
+    toggleEventListener(false);
+  });
 }
