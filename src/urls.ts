f682c2383a3e19d2783d2dfb6238b5ee92ca2bd9
Grzegorz Łubian <46636403+glubian@users.noreply.github.com>
2023-05-02 17:25:43 +0200

    Update URLs

--- src/urls.ts
+++ src/urls.ts
@@ -2,7 +2,7 @@
 export const BASE_URL = "http://localhost:3333";
 
 /** WebSocket server URL. */
-export const WS_SERVER_URL = "wss://localhost:8080/ws";
+export const WS_SERVER_URL = "wss://localhost:8080";
 
 /** Invite link lobby parameter. */
 export const URL_LOBBY_PARAMETER = "lobby";
